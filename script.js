d54a508c1d36bf46dae73d10edd1921fbb46524a
nrdj <nriedeldj@gmail.com>
2022-01-03 14:32:29 -0300

    Js Market value
    Calculates and sets the market value based on sharesOwned & marketPrice

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('tbody tr').each(function (i, ele) {
+      var sharesOwned = parseFloat($(ele).children('.shares').text());
+      var marketPrice = parseFloat($(ele).children('.marketPrice').text());
+  
+      // market value is shares times market price per share
+      var marketValue = sharesOwned * marketPrice;
+      $(ele).children('.marketValue').html(marketValue);
+    });
+  });
